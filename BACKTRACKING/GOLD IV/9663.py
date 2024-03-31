ad6d4fb16d763279424e3160f93219b2a8064d82
Songminseok <songms0909@khu.ac.kr>
2024-03-31 14:56:43 +0900

    2024-03-31

---
+++ BACKTRACKING/GOLD IV/9663.py
@@ -0,0 +1,27 @@
+import sys
+
+n = int(sys.stdin.readline())
+result = 0
+col = [0] * n
+
+def isPromissing(x):
+    for i in range(x):
+        if col[x] == col[i] or abs(col[x] - col[i]) == abs(x - i):
+            return False
+    
+    return True
+
+def nQueens(x):
+    global result
+    if x == n:
+        result += 1
+        return
+
+    else:
+        for i in range(n):
+            col[x] = i #퀸을 놓음
+            if isPromissing(x):
+                nQueens(x+1)
+
+nQueens(0)
+print(result)
