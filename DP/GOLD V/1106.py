ae15651704b44b403d59e8b9cd7263f0ac34b32d
songminseok <songms0909@khu.ac.kr>
2024-04-12 02:01:32 +0900

    2024-04-12

--- DP/GOLD V/1106.py
+++ DP/GOLD V/1106.py
@@ -1,13 +1,12 @@
 customers, city = map(int, input().split())
 inputArr = [list(map(int, input().split()))for _ in range(city)]
 result = int(1e9)
-dp = [int(1e9)] * (city+100)
+dp = [int(1e9)] * (customers+100)
 dp[0] = 0
 
 for cost, customer in inputArr:
     for i in range(customer, len(dp)):
         dp[i] = min(dp[i-customer] + cost, dp[i])
-        if i >= customers :
-            result = min(result, dp[i])
+       
 
-print(result)
+print(min(dp[customers:]))
