61583a1c10ca7d61a774a0071f57d8ad76def835
songminseok <songms0909@khu.ac.kr>
2024-04-13 19:31:26 +0900

    2024-04-13

---
+++ ETC/SILVER III/1463.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/1463
+
+
+N = int(input())
+
+dp = [0] * 1000001
+
+for i in range(2, N+1):
+    dp[i] = dp[i-1] + 1
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2] + 1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+
+print(dp[N])
+
+
