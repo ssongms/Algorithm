ce26d1173ca5f101664412650516e99ebdfc1a62
송민석 <songms0909@khu.ac.kr>
2024-04-19 00:44:13 +0900

    2024-04-19

---
+++ ETC/SILVER V/2097.py
@@ -0,0 +1,14 @@
+import math
+N = int(input())
+sqrtN = math.ceil(math.sqrt(N)) # 제곱근의 올림
+
+if N <= 4:
+    print(4)
+else:
+    width = sqrtN-1
+    height = 0
+    while True:
+        N -= sqrtN
+        height += 1
+        if N <= sqrtN: break 
+    print((height+width)*2)
