561cedb2114fb25600c3c3a18428187ab6ba40a1
songminseok <songms0909@khu.ac.kr>
2024-05-07 14:22:44 +0900

    2024-05-07

--- DFS/SIVER I/11403.py
+++ DFS/SIVER I/11403.py
@@ -1,13 +1,14 @@
-node = int(input())
-graph = [list(map(int, input().split())) for _ in range(node)]
-visited = [[0 for _ in range(node)] for _ in range(node)]
-# visited = [0] * node
+def floyd(node, graph):
+    for i in range(node):
+        for j in range(node):
+            for k in range(node):
+                if graph[j][i] == 1 and graph[i][k] == 1: # j에서 i까지 연결돼있고, i에서 k까지 연결돼있다면
+                    graph[j][k] = 1 # j와 k도 연결돼있음 (여기서 i는 중간 노드의 역할)
 
-for k in range(node):
-    for a in range(node):
-        for b in range(node):
-            if graph[a][b] == 1 or graph[a][k] == 1 or graph[k][b] == 1:
-                graph[a][b] = 1
+# main 
+node = int(input()) # 노드의 개수 입력
+adjacentGraph = [list(map(int, input().split())) for _ in range(node)] # 인접 그래프 입력
+floyd(node, adjacentGraph) # 플로이드 워셜 함수 호출
         
-
-print(graph)
+for line in adjacentGraph: # 출력문
+    print(*line)
