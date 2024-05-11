0d6b315cda54bd510d18bb71b44183ef601b2de1
송민석 <songms0909@khu.ac.kr>
2024-05-11 15:46:30 +0900

    2024-05-11

--- DFS/SIVER I/1389.py
+++ DFS/SIVER I/1389.py
@@ -1,23 +1,31 @@
 import sys
-from collections import deque
 
 N, M = map(int, sys.stdin.readline().split())
-adjGraph = [[0 for _ in range(N+1)] for _ in range(N+1)]
+adjGraph = [[int(1e9) for _ in range(N+1)] for _ in range(N+1)]
 
 def kevinBacon(N, adj):
-    kevin = [[0 for _ in range(N+1)] for _ in range(N+1)]
-    for i in range(N+1):
-        for j in range(N+1):
-            for k in range(N+1):
-                if adj[j][i] == 1 and adj[i][k] == 1:
-                    kevin[j][k] += 1
-    for line in kevin:
-        print(*line)
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            for k in range(1, N+1):
+                if j == k:
+                    adj[j][k] = 0
+                else:
+                    adj[j][k] = min(adj[j][k], adj[j][i] + adj[i][k])
+    newGraph = []
+    for i in range(1, N+1):
+        slicedLine = adj[i][1:]
+        newGraph.append(slicedLine)
 
+    answer = []
+    for row in newGraph:
+        answer.append(sum(row))
+    
+    result = answer.index(min(answer)) + 1
+    return result
 
 for i in range(M): # 인접 그래프 입력
     a, b = map(int, sys.stdin.readline().split())
     adjGraph[a][b] = 1
     adjGraph[b][a] = 1
 
-kevinBacon(N, adjGraph)
+print(kevinBacon(N, adjGraph))
